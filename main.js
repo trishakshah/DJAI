901ae41880ebbc41343b5ea0d4c997dfda6ff677
trishakshah <79252404+trishakshah@users.noreply.github.com>
2021-12-05 15:03:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 var dionysus="";
 var makeItRight="";
-var leftWristX="";
-var leftWristY="";
-var rightWristX="";
-var rightWristY="";
-var noseX="";
-var noseY="";
-var leftWristC="";
-var rightWristC="";
+
+function preload(){
+    dionysus=loadSound("dionysus.mp3");
+    makeItRight=loadSound("makeItRight.mp3")
+}
+
+var leftWristX=0;
+var leftWristY=0;
+var rightWristX=0;
+var rightWristY=0;
+var noseX=0;
+var noseY=0;
+var leftWristC=0;
+var rightWristC=0;
 
 function setup(){
     canvas=createCanvas(600,400);
@@ -18,9 +24,22 @@ function setup(){
     model.on("pose",getResults);
 }
 
-function preload(){
-    dionysus=loadSound("dionysus.mp3");
-    makeItRight=loadSound("makeItRight.mp3")
+function modelLoaded(){
+    console.log("Model loaded.")
+} 
+
+function getResults(results){
+    if (results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        rightWristX=results[0].pose.rightWristX.x;
+        rightWristY=results[0].pose.rightWristY.y;
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        rightWristC=results[0].pose.keypoints[10].score;
+        leftWristC=results[0].pose.keypoints[9].score;
+    }
 }
 
 function draw(){
@@ -39,22 +58,27 @@ function draw(){
         if (rightWristY>0 && rightWristY<=80){
             dionysus.setVolume(0.2);
             makeItRight.setVolume(0.2);
+            document.getElementById("volumeOutput").innerHTML="0.2";
         }
-        if (rightWristY>80 && rightWristY<=160){
+        else if (rightWristY>80 && rightWristY<=160){
             dionysus.setVolume(0.4);
             makeItRight.setVolume(0.4);
+            document.getElementById("volumeOutput").innerHTML="0.4"
         }
-        if (rightWristY>160 && rightWristY<=240){
+        else if (rightWristY>160 && rightWristY<=240){
             dionysus.setVolume(0.6);
             makeItRight.setVolume(0.6);
+            document.getElementById("volumeOutput").innerHTML="0.6"
         }
-        if (rightWristY>240 && rightWristY<=320){
+        else if (rightWristY>240 && rightWristY<=320){
             dionysus.setVolume(0.8);
             makeItRight.setVolume(0.8);
+            document.getElementById("volumeOutput").innerHTML="0.8"
         }
-        if (rightWristY>320 && rightWristY<=400){
+        else if (rightWristY>320 && rightWristY<=400){
             dionysus.setVolume(1);
             makeItRight.setVolume(1);
+            document.getElementById("volumeOutput").innerHTML="1.0"
         }
     }
     if (leftWristC>0.2){
@@ -62,39 +86,25 @@ function draw(){
         if (leftWristY>0 && leftWristY<=100){
             dionysus.rate(0.5);
             makeItRight.rate(0.5);
+            document.getElementById("speedOutput").innerHTML="0.5x"
         }
         if (leftWristY>100 && leftWristY<=200){
             dionysus.rate(1);
             makeItRight.rate(1);
+            document.getElementById("speedOutput").innerHTML="1.0x"
         }
         if (leftWristY>200 && leftWristY<=300){
             dionysus.rate(1.5);
             makeItRight.rate(1.5);
+            document.getElementById("speedOutput").innerHTML="1.5x"
         }
         if (leftWristY>300 && leftWristY<=400){
             dionysus.rate(2);
             makeItRight.rate(2);
+            document.getElementById("speedOutput").innerHTML="2.0x"
         }
     }
     circle(noseX,noseY,20);
-}
-
-function modelLoaded(){
-    console.log("Model loaded.")
-}
-
-function getResults(results){
-    if (results.length>0){
-        console.log(results);
-        leftWristX=results[0].pose.leftWrist.x;
-        leftWristY=results[0].pose.leftWrist.y;
-        leftWristC=results[0].pose.keypoints[9].score;
-        rightWristX=results[0].pose.rightWristX.x;
-        rightWristY=results[0].pose.rightWristY.y;
-        rightWristC=results[0].pose.keypoints[10].score;
-        noseX=results[0].pose.nose.x;
-        noseY=results[0].pose.nose.y;
-    }
 }
 
 function playMusic(){
@@ -102,12 +112,17 @@ function playMusic(){
         makeItRight.stop();
         dionysus.play();
         dionysus.setVolume(1);
-        song.rate(1);
+        dionysus.rate(1);
     }
     if (noseX>300){
         dionysus.stop();
         makeItRight.play();
         makeItRight.setVolume(1);
-        song.rate(1);
+        makeItRight.rate(1);
     }
+}
+
+function stopMusic(){
+    dionysus.stop();
+    makeItRight.stop();
 }
